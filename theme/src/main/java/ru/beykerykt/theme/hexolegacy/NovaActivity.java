029a8ca9eecf46bcc45bfe69211608d243701317
BeYkeRYkt <beykerykt@gmail.com>
2016-10-18 01:55:17 +0700

    Format

--- theme/src/main/java/ru/beykerykt/theme/hexolegacy/NovaActivity.java
+++ theme/src/main/java/ru/beykerykt/theme/hexolegacy/NovaActivity.java
@@ -1,16 +1,14 @@
 package com.example.novatheme;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 
-public class NovaActivity extends Activity
-{
-    /** Called when the activity is first created. */
+public class NovaActivity extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
     @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
+    public void onCreate(Bundle savedInstanceState) {
         //super.onCreate(savedInstanceState);
     }
 
